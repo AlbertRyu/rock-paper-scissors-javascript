b3b998dc1a263236400db91cb9595d75c8cb4575
Yunxiao Liu <yunxiao.liu@tum.de>
2025-03-20 17:20:49 +0100

    getComputerChoice function worked

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("hello world!")
+
+function getComputerChoice() {
+    let random_number = Math.random()
+    console.log(random_number)
+    if (random_number < 0.33) {
+        return "Rock"
+    } else if (random_number > 0.67) {
+        return "Scissors"
+    } else {
+        return "Paper"
+    }
+}
+
+console.log(getComputerChoice())
