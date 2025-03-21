c0a074bcbf3207c01a32c1e7c6af19fcfdd67bee
Yunxiao Liu <yunxiao.liu@tum.de>
2025-03-21 22:05:11 +0100

    playGame function worked. Everything worked.

--- script.js
+++ script.js
@@ -22,45 +22,60 @@ function takeCareFormate(choice){
     return choice[0].toUpperCase() + choice.slice(1).toLowerCase()
 }
 
-let humanScore = 0
-let computerScore = 0
 
-function playRound(humanChoice, computerChoice){
 
-    function lose(){
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
-        computerScore++
+function playGame() {
+
+    let humanScore = 0
+    let computerScore = 0
+
+    function playRound(humanChoice, computerChoice){
+
+        function lose(){
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+            computerScore++
+        }
+
+        function win(){
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+            humanScore++
+        }
+
+        function tie(){
+            console.log("It's a tie. No one scores.")
+        }
+
+        if (humanChoice == 'Rock') {
+            if (computerChoice == 'Paper') {lose()} 
+            else if (computerChoice == 'Scissors') {win()} 
+            else {tie()}
+        } else if (humanChoice == 'Paper') {
+            if (computerChoice == 'Scissors') {lose()} 
+            else if (computerChoice == 'Rock') {win()} 
+            else {tie()} 
+        } else if (humanChoice == 'Scissors') {
+            if (computerChoice == 'Rock') {lose()} 
+            else if (computerChoice == 'Paper') {win()} 
+            else {tie()}
+        } else {console.log('Invalid Input, Invalid game.')}
+
+        console.log(`After this match, the score is
+            Computer: ${computerScore}
+            Player:   ${humanScore}`)
     }
 
-    function win(){
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`)
-        humanScore++
+    let humanChoice, computerChoice, highestScore
+    highestScore = 0
+    while (highestScore < 5){
+        humanChoice = getHumanChoice()
+        computerChoice = getComputerChoice()
+        playRound(humanChoice, computerChoice)
+        highestScore = Math.max(humanScore,computerScore)
     }
 
-    function tie(){
-        console.log("It's a tie. No one scores.")
-    }
+    if (humanScore == 5) {console.log("The Player wins!")} 
+    else { console.log("The Computer wins!")}
 
-    if (humanChoice == 'Rock') {
-        if (computerChoice == 'Paper') {lose()} 
-        else if (computerChoice == 'Scissors') {win()} 
-        else {tie()}
-    } else if (humanChoice == 'Paper') {
-        if (computerChoice == 'Scissors') {lose()} 
-        else if (computerChoice == 'Rock') {win()} 
-        else {tie()} 
-    } else if (humanChoice == 'Scissors') {
-        if (computerChoice == 'Rock') {lose()} 
-        else if (computerChoice == 'Paper') {win()} 
-        else {tie()}
-    } else {console.log('Invalid Input, Invalid game.')}
-
-    console.log(`After this match, the score is
-        Computer: ${computerScore}
-        Player:   ${humanScore}`)
 }
 
-let humanChoice = getHumanChoice()
-let computerChoice = getComputerChoice()
-
-playRound(humanChoice, computerChoice)
+playGame()
