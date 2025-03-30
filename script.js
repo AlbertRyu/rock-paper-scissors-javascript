35560ca8f56c7851a5dc91f5678009ff887ed33a
Yunxiao <yunxiao.liu@proton.me>
2025-03-30 23:47:40 +0200

    the game is playable but do not end properly

--- script.js
+++ script.js
@@ -11,71 +11,73 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice(){
-    let choice = prompt("What is your choice?")
-    choice = takeCareFormate(choice)
-    return choice
-}
-
 function takeCareFormate(choice){
     // This function takes care about the format.
     return choice[0].toUpperCase() + choice.slice(1).toLowerCase()
 }
 
+let humanScore = 0
+let computerScore = 0
 
+function playRound(humanChoice, computerChoice){
+    
 
-function playGame() {
-
-    let humanScore = 0
-    let computerScore = 0
-
-    function playRound(humanChoice, computerChoice){
-
-        function lose(){
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
-            computerScore++
-        }
-
-        function win(){
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`)
-            humanScore++
-        }
-
-        function tie(){
-            console.log("It's a tie. No one scores.")
-        }
-
-        if (humanChoice == 'Rock') {
-            if (computerChoice == 'Paper') {lose()} 
-            else if (computerChoice == 'Scissors') {win()} 
-            else {tie()}
-        } else if (humanChoice == 'Paper') {
-            if (computerChoice == 'Scissors') {lose()} 
-            else if (computerChoice == 'Rock') {win()} 
-            else {tie()} 
-        } else if (humanChoice == 'Scissors') {
-            if (computerChoice == 'Rock') {lose()} 
-            else if (computerChoice == 'Paper') {win()} 
-            else {tie()}
-        } else {console.log('Invalid Input, Invalid game.')}
-
-        console.log(`After this match, the score is
-            Computer: ${computerScore}
-            Player:   ${humanScore}`)
+    function lose(){
+        resultText = `You lose! ${computerChoice} beats ${humanChoice}.`
+        computerScore++
     }
 
-    let humanChoice, computerChoice, highestScore
-    highestScore = 0
-    while (highestScore < 5){
-        humanChoice = getHumanChoice()
-        computerChoice = getComputerChoice()
-        playRound(humanChoice, computerChoice)
-        highestScore = Math.max(humanScore,computerScore)
+    function win(){
+        resultText = `You win! ${humanChoice} beats ${computerChoice}.`
+        humanScore++
     }
 
-    if (humanScore == 5) {console.log("The Player wins!")} 
-    else { console.log("The Computer wins!")}
+    function tie(){
+        resultText = "It's a tie. No one scores."
+    }
+
+    if (humanChoice == 'Rock') {
+        if (computerChoice == 'Paper') {lose()} 
+        else if (computerChoice == 'Scissors') {win()} 
+        else {tie()}
+    } else if (humanChoice == 'Paper') {
+        if (computerChoice == 'Scissors') {lose()} 
+        else if (computerChoice == 'Rock') {win()} 
+        else {tie()} 
+    } else if (humanChoice == 'Scissors') {
+        if (computerChoice == 'Rock') {lose()} 
+        else if (computerChoice == 'Paper') {win()} 
+        else {tie()}
+    } else {resultText = 'Invalid Input, Invalid game.'}
+
+    const matchResult = document.querySelector('.matchResult')
+    const currentScore = document.querySelector('.currentScore')
+    scoreText = `
+        After this match, the score is <br>
+        Computer: ${computerScore}
+        Player:   ${humanScore}`
+
+    matchResult.textContent = resultText
+    currentScore.innerHTML = scoreText
 
 }
 
-playGame()
+let humanChoice, computerChoice, highestScore
+highestScore = 0
+const buttons = document.querySelectorAll('.choice')
+buttons.forEach(
+    button => button.addEventListener('click', (e) => humanMadeAChoice(e.target.textContent))
+)
+function humanMadeAChoice(humanChoice){
+    computerChoice = getComputerChoice()
+    playRound(humanChoice, computerChoice)
+    highestScore = Math.max(humanScore,computerScore)
+    let status;
+    if (humanScore == 5) {
+        document.querySelector('.finalMessage').innerHTML = `You win the whole game!`
+    }else if (computerScore == 5) {
+        document.querySelector('.finalMessage').innerHTML = `You lose the whole game!`
+    }
+}
+
+//playGame()
